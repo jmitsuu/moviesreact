acc119eb23b152e38a0c0daa6d7bcce01eaf9416
jmitsuu <jmiguelafonso@hotmail.com>
2024-07-15 17:59:20 -0300

    conf

--- src/components/review/CardReview.tsx
+++ src/components/review/CardReview.tsx
@@ -9,7 +9,7 @@ export function CardReview({ backdrop_path }: Review) {
   const urlImage = 'https://image.tmdb.org/t/p/original'
   const url = urlImage + backdrop_path
   return (
-    <div className="absolute">
+    <div className="absolute hover:border hover:border-blue-500 hover:z-50">
       <div className=" flex  w-[700px]">
         <img src={url} alt="" className="  " />
       </div>
